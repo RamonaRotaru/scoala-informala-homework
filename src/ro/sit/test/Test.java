ec26f94a1f41f8e1f0bf20c9ba5356092e1f31f1
Ramona Rotaru <sia_ramona@yahoo.com>
2019-07-08 20:09:07 +0300

    Added project

--- src/ro/sit/test/Test.java
+++ src/ro/sit/test/Test.java
@@ -3,5 +3,8 @@ package ro.sit.test;
 public class Test {
     public static void main(String[] args) {
         System.out.println("test");
+        for( int i =0; i< 10;i++){
+            System.out.print("test1");
+        }
     }
 }
